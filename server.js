59ccedee5b96d82f57044b80b88114ea3fcd6577
snjytwri <sanjay.tiwari@tribridge.com>
2017-04-12 16:09:43 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const hbs  = require('hbs');
+const fs = require('fs')
+
+
+var app = express();
+hbs.registerPartials(__dirname+"/views/partials")
+app.set('view engine','hbs');
+app.use(express.static(__dirname + "/public"));
+
+app.use((req,res,next)=>{
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + "\n",(err)=>{
+        if(err){
+            console.log(err.message)
+        };
+    })
+
+next()
+});
+
+// app.use((req,res,next)=>{
+//     res.render('maintenance.hbs')
+// next();
+// });
+
+
+
+app.get('/',(req,res)=>{
+    res.render('home.hbs',{
+    pageTitle: 'Welcome',
+    welcomeText:'This is a page for the udemy node.js tutorial by sanjay',
+    currYear: new Date().getFullYear()
+});
+   
+});
+
+app.get('/about',(req,res)=>{
+res.render('about.hbs',{
+    pageTitle: 'About Page',
+    currYear: new Date().getFullYear()
+});
+});
+
+app.get('/bad',(req,res)=>{
+res.send({
+    errorMessage: 'Unable to process request'
+});
+});
+
+app.listen(3000);
